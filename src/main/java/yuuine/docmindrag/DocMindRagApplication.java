afe72c40bb2bba5b521610a31d603472ca398b51
yunqing <jzhang0703@163.com>
2026-04-01 23:12:55 +0800

    feat(project): 初始化项目结构

---
+++ src/main/java/yuuine/docmindrag/DocMindRagApplication.java
@@ -0,0 +1,13 @@
+package yuuine.docmindrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocMindRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocMindRagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yuuine/docmindrag/DocMindRagApplicationTests.java
@@ -0,0 +1,13 @@
+package yuuine.docmindrag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocMindRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
